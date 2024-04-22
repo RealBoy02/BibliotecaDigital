9f02ed121a146845c08298387902b07c842499f9
RealBoy02 <146474724+RealBoy02@users.noreply.github.com>
2024-04-21 23:06:44 -0600

    Add files via upload

---
+++ db.php
@@ -0,0 +1,7 @@
+<?php
+
+$conexion = mysqli_connect("localhost", "root", "","libros");
+//$conexion = mysqli_connect("127.0.0.1", "root", "","libros");
+//conexion de base de datos de la universidad $conexion = mysqli_connect("173.17.26.146", "yais", "cisco","libros");
+$conexion ->set_charset("utf8");
+?>
